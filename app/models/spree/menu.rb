7ae717eb0de932be1314d6ba747b7a69af25addb
Nick Herro <ncherro@gmail.com>
2013-09-18 13:03:50 -0400

    cache

--- app/models/spree/menu.rb
+++ app/models/spree/menu.rb
@@ -1,20 +1,31 @@
 class Spree::Menu < ActiveRecord::Base
 
+  SP_CACHE_PREFIX = "sp_menu_"
+  SP_CACHE_KEYS = "sp_menu_keys"
+
   attr_accessible :title
 
   has_many :pages, class_name: "Spree::Page", foreign_key: "spree_menu_id", dependent: :destroy
 
+  after_save :clear_menu_cache
+
   def options_for_select(page_id=nil)
-    # used for admin forms - shows all items
-    q = pages.select('id, title, slug, foreign_link, parent_id, visible')
-    arr = flatten(q.as_json(root: false))
-    r = []
-    arr.each do |item|
-      option = ["#{'-' * item[:parents].length } #{item[:title]}#{ ' (not visible)' unless item[:visible]}", item[:id]]
-      option << { disabled: "disabled" } if page_id && (item[:id] == page_id || item[:parent_ids].include?(page_id))
-      r << option
+    key = "#{SP_CACHE_PREFIX}options_for_select_#{self.id}_#{page_id}"
+    Rails.cache.fetch(key) do
+      # add this to our cache of cache keys for deletion
+      self.class.cache_key(key)
+      # used for admin forms - shows all items
+      q = pages.select('id, title, slug, foreign_link, parent_id, visible')
+      arr = flatten(q.as_json(root: false))
+      r = []
+      arr.each do |item|
+        option = ["#{'-' * item[:parents].length } #{item[:title]}#{ ' (not visible)' unless item[:is_visible]}", item[:id]]
+        # page cannot be a descendent of itself, or of it's children
+        option << { disabled: "disabled" } if page_id && (item[:id] == page_id || item[:parent_ids].include?(page_id))
+        r << option
+      end
+      r
     end
-    r
   end
 
   def nested_items(*args)
@@ -25,15 +36,44 @@ class Spree::Menu < ActiveRecord::Base
     }
     options = defaults.merge(args.extract_options!)
 
-    q = pages.select('id, title, slug, foreign_link, parent_id, visible')
-    q = q.visible if options[:only_visible]
-    flatten(q.as_json(root: false), show_children: true, max_levels: options[:max_levels], current_path: options[:current_path])
+    key = "#{SP_CACHE_PREFIX}nested_items_#{self.id}_#{options[:only_visible]}_#{options[:max_levels]}_#{options[:current_path]}"
+    Rails.cache.fetch(key) do
+      # add this to our cache of cache keys for deletion
+      self.class.cache_key(key)
+      q = pages.select('id, title, slug, foreign_link, parent_id, visible')
+      q = q.visible if options[:only_visible]
+      flatten(q.as_json(root: false), show_children: true, max_levels: options[:max_levels], current_path: options[:current_path])
+    end
   end
 
   class << self
     def options_for_select
       opts = [['- no menu -', nil,]]
       opts += Spree::Menu.all.collect{ |i| [i.title, i.id] }
+    end
+
+    def cache_key(key)
+      Rails.logger.debug("writing menu cache to #{key}")
+      # workaround for lack of delete_matched functionality with memcached
+      # tracks all of our menu cache keys so we can delete them later
+      keys = Rails.cache.fetch(SP_CACHE_KEYS) do
+        []
+      end
+      unless keys.include?(key)
+        keys << key
+        Rails.cache.write(SP_CACHE_KEYS, keys)
+      end
+    end
+
+    def clear_caches
+      keys = Rails.cache.read(SP_CACHE_KEYS) do
+        []
+      end
+      keys.each do |key|
+        Rails.cache.delete(key)
+      end
+      # and overwrite our keys
+      Rails.cache.write(SP_CACHE_KEYS, [])
     end
   end
 
@@ -64,6 +104,7 @@ class Spree::Menu < ActiveRecord::Base
           link: page['foreign_link'].present? ? page['foreign_link'] : "/#{page['slug']}",
           parents: options[:parents],
           parent_ids: options[:parent_ids],
+          is_visible: page['visible'],
           is_on: page['is_on'],
           is_active: page['is_active'],
         }
